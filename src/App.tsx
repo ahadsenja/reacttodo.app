1a39d34e73dee8ba73a41cf1906c8ea9cab84cdd
ahadsenja <hamdanilm96@gmail.com>
2023-04-09 20:48:54 +0800

    create todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <>
+      <h1>Todo App</h1>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="todo">Add Todo</label>
+          <input className="form-control" type="text" id="todo" />
+        </div>
+        <button type="submit" className="btn">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      </form>
+
+      <hr />
+      <h2>List Todo</h2>
+
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-export default App
+export default App;
